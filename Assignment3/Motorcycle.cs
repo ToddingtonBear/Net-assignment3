5ec8765a0311a03559438c872c101daa7be3fa61
Chris <chris.todd.tbp@outlook.com>
2019-02-01 13:13:26 +0000

    added motorcycle

---
+++ Assignment3/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+    class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+            DOORS = 0;
+            SEATS = 1;
+            WHEELS = 2;
+        }
+
+        public override void Rev()
+        {
+            Console.WriteLine("Neeeoooowwww");
+        }
+    }
+}
