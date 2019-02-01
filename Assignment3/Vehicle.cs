9ad64df922e6d09a2dcef9f3c63e205e4cf40828
Chris <chris.todd.tbp@outlook.com>
2019-02-01 12:28:01 +0000

    added vehicle class

---
+++ Assignment3/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+    class Vehicle
+    {
+        protected int doors = 4;
+        protected int seats = 5;
+        protected int wheels = 4;
+
+        public int DOORS
+        {
+            get { return doors; }
+            set { doors = value; }
+        }
+        public int SEATS
+        {
+            get { return seats; }
+            set { seats = value; }
+        }
+        public int WHEELS
+        {
+            get { return wheels; }
+            set { wheels = value; }
+        }
+
+        public Vehicle() { }
+
+        public virtual void Specs()
+        {
+            Console.WriteLine(doors + " seats");
+            Console.WriteLine(seats + " seats");
+            Console.WriteLine(wheels + " wheels");
+        }
+
+        public virtual void Rev()
+        {
+            Console.WriteLine("rumble");
+        }
+    }
+}
